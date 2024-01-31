1a4d8ee32dc9382c9fe9211936ee36524fd5bbb6
Crowdbotics <team@crowdbotics.com>
2024-01-31 08:46:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-mouse-45712",
+  url: "https://blue-mouse-45712.botics.co",
+  api: "https://blue-mouse-45712.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
